20906ab33f38cd8bb528d475eb498c0cf476a5be
Mikhail Katychev <mkatych@gmail.com>
2025-09-29 13:36:28 -0500

    clippy fix

--- src/server/response_item.rs
+++ src/server/response_item.rs
@@ -212,11 +212,11 @@ impl Item {
         };
 
         let extract_name = |node: &Node, name| {
-            let res = node
-                .child_by_field_name(name)
-                .map(|child| node_text(code, &child).to_owned());
+            
             // log_to_console!("{} {:?}", name, res);
-            res
+            node
+                .child_by_field_name(name)
+                .map(|child| node_text(code, &child).to_owned())
         };
 
         let kind = node.kind();
